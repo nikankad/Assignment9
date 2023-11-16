9c18bc6a09668f3e13378d0b24574fa929298e50
Nikan <nikankad@protonmail.com>
2023-11-16 15:31:45 -0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class Main {
+   static Hashtable<String, SaleRecord> hashTable = new Hashtable<>(15);
+    static ArrayList<SaleRecord> arrayList = new ArrayList<SaleRecord>();
+
+    public static void insert(String csvFile) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src/small_sample.csv"))) {
+            br.readLine(); // Skip the header row
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                // Assuming the order of columns in the CSV matches the constructor
+                final SaleRecord saleRecord = new SaleRecord(data[0], data[1], data[2], data[3], data[4], Integer.parseInt(data[5]), Double.parseDouble(data[6]), Double.parseDouble(data[7]), Double.parseDouble(data[8]));
+                // Gets the customer last name from csv
+                String customerLastName = data[2].substring(data[2].lastIndexOf(" ") + 1);
+                hashTable.put(customerLastName, saleRecord);
+//                System.out.println(hashTable.hashCode());
+                arrayList.add(saleRecord);
+                // Insert the SaleRecord into the corresponding AVL tree based on Car Make
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        insert("s");
+    }
+}
---
+++ src/SaleRecord.java
@@ -0,0 +1,85 @@
+import java.util.Date;
+
+public class SaleRecord {
+    private String date;
+    private String salesPerson;
+    private String customerName;
+    private String carMake;
+    private String carModel;
+
+    private int carYear;
+    private double salePrice;
+    private double comissionRate;
+    private double comissionEarned;
+
+
+    public SaleRecord(String date, String salePerson, String customerName, String carMake, String carModel, int carYear,
+                      double salePrice, double comissionRate, double comissionEarned) {
+        this.date = date;
+        this.salesPerson = salePerson;
+        this.customerName = customerName;
+        this.carMake = carMake;
+        this.carModel = carModel;
+        this.carYear = carYear;
+        this.salePrice = salePrice;
+        this.comissionRate = comissionRate;
+        this.comissionEarned = comissionEarned;
+
+    }
+
+
+    public SaleRecord(Date date2, String salesperson2, String customerName2, String carMake2, String carModel2,
+                      int carYear2, double salePrice2, double commissionRate) {
+    }
+
+
+    /**
+     * @return String
+     */
+    public String getSalePerson() {
+        return salesPerson;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int carYear() {
+        return carYear;
+    }
+
+    public double getCommissionRate() {
+        return comissionRate;
+    }
+
+    public String getModel() {
+        return carModel;
+    }
+
+    public String getMake() {
+        return carMake;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    @Override
+    public String toString() {
+        return salesPerson + ", " + customerName + ", " + carMake + ", " + carModel + ", " + carYear + ", " + salePrice
+                + ", " + comissionRate + ", " + comissionEarned;
+    }
+
+    public double getCommissionEarned() {
+        return comissionEarned;
+    }
+
+    @Override
+    public int hashCode(){
+        return customerName.substring(customerName.lastIndexOf(" ") + 1).hashCode();
+    }
+}
